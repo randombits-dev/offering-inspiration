6eff62011463745eed4d4f82c49a5fb7867f6611
Matthew Nienow <mnienow@pm.me>
2023-11-30 21:08:30 -0600

    Added ability to write to files

--- src/pages/save.ts
+++ src/pages/save.ts
@@ -1,13 +1,30 @@
 import type {APIRoute} from "astro";
+import fs from "fs-extra";
 
 export const prerender = false;
-export async const POST: APIRoute = ({request}) => {
-  const body = request.json();
-  if (body.buyable) {
 
+interface Body {
+    buyable?: string[];
+    non?: string[];
+    bad?: string[];
+}
+
+export const POST: APIRoute = async ({request}) => {
+  const body: Body = await request.json();
+  if (body.buyable && body.buyable.length > 0) {
+    const existing = JSON.parse(fs.readFileSync('src/data/buyable.json', 'utf8'));
+    const newBuyable = [...existing, ...body.buyable];
+    fs.writeFileSync('src/data/buyable.json', JSON.stringify(newBuyable));
   }
-  return new Response(JSON.stringify({
-        message: "This was a POST!"
-      })
-  );
+  if (body.non && body.non.length > 0) {
+    const existing = JSON.parse(fs.readFileSync('src/data/not.json', 'utf8'));
+    const newNon = [...existing, ...body.non];
+    fs.writeFileSync('src/data/not.json', JSON.stringify(newNon));
+  }
+    if (body.bad && body.bad.length > 0) {
+        const existing = JSON.parse(fs.readFileSync('src/data/bad.json', 'utf8'));
+        const newBad = [...existing, ...body.bad];
+        fs.writeFileSync('src/data/bad.json', JSON.stringify(newBad));
+    }
+  return new Response(null, {status: 204});
 };
