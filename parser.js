23a606485cf341345f01fc92df3a40278fb18a2d
nienow <mnienow@pm.me>
2023-12-02 19:07:37 -0600

    First version of randomly generated page

--- parser.js
+++ parser.js
@@ -1,57 +1,63 @@
 import fs from 'fs';
 
-const parseFile = (fileName) => {
-  const data = fs.readFileSync(fileName, 'utf8');
-  const lines = data.split('\n');
+const buyable = JSON.parse(fs.readFileSync("src/data/buyable.json", 'utf8'));
+const buyMap = {};
+buyable.forEach(s => buyMap[s.id] = s);
 
-  const projects = [];
-  const projectsWithUrl = [];
-  const categories = {};
+const data = fs.readFileSync('raw/Kickstarter_2023-11-16T03_20_10_045Z.json', 'utf8');
+const lines = data.split('\n');
 
-  lines.forEach((line) => {
-    try {
-      const project = JSON.parse(line);
-      const data = project.data;
-      if (data.state === 'successful') {
+const projects = [];
+const projectsWithUrl = [];
+const categories = {};
+const buyableProjects = [];
 
-        const item = {
-          id: data.id,
-          name: data.name,
-          blurb: data.blurb,
-          url: data.profile.link_url,
-          cat: data.category.parent_name,
-          cat2: data.category.name,
-          funded: data.deadline,
-          pledged: data.usd_pledged,
-          backers: data.backers_count,
-          imgs: {
-            full: data.photo.full,
-            sm: data.photo.little
-          }
-        };
-        projects.push(item);
+lines.forEach((line) => {
+  try {
+    const project = JSON.parse(line);
+    const data = project.data;
+    if (data.state === 'successful') {
 
-        if (data.profile.link_url) {
-          projectsWithUrl.push(item);
+      const item = {
+        id: data.id,
+        name: data.name,
+        blurb: data.blurb,
+        url: data.profile.link_url,
+        cat: data.category.parent_name,
+        cat2: data.category.name,
+        funded: data.deadline,
+        pledged: data.usd_pledged,
+        backers: data.backers_count,
+        imgs: {
+          full: data.photo.full,
+          sm: data.photo.little
         }
+      };
+      projects.push(item);
 
-        if (!categories[item.cat]) {
-          categories[item.cat] = new Set();
-        }
-        categories[item.cat].add(item.cat2);
+      if (data.profile.link_url) {
+        projectsWithUrl.push(item);
       }
-    } catch (e) {
-      console.log('Error parsing JSON', e);
+
+      if (!categories[item.cat]) {
+        categories[item.cat] = new Set();
+      }
+      categories[item.cat].add(item.cat2);
+
+      if (buyMap[item.id]) {
+        buyableProjects.push({...item, ...buyMap[item.id]});
+      }
     }
-  });
+  } catch (e) {
+    console.log('Error parsing JSON', e);
+  }
+});
 
-  const catOut = Object.entries(categories).map(([key, value]) => {
-    return {name: key, sub: new Array(...value)};
-  });
+const catOut = Object.entries(categories).map(([key, value]) => {
+  return {name: key, sub: new Array(...value)};
+});
 
-  fs.writeFileSync('src/data/categories.json', JSON.stringify(catOut));
-  fs.writeFileSync('src/data/projects.json', JSON.stringify(projects));
-  fs.writeFileSync('src/data/projectsWithUrl.json', JSON.stringify(projectsWithUrl));
-};
-
-parseFile('raw/Kickstarter_2023-11-16T03_20_10_045Z.json');
+fs.writeFileSync('src/data/categories.json', JSON.stringify(catOut));
+fs.writeFileSync('src/data/projects.json', JSON.stringify(projects));
+fs.writeFileSync('src/data/projectsWithUrl.json', JSON.stringify(projectsWithUrl));
+fs.writeFileSync('src/data/buyableProjects.json', JSON.stringify(buyableProjects));
