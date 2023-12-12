d1af753a093d40092a867202b3d85373f5b50844
nienow <mnienow@pm.me>
2023-12-11 21:18:02 -0600

    Added api for reporting

---
+++ api/src/index.ts
@@ -0,0 +1,59 @@
+export interface Env {
+	GIFT: R2Bucket;
+}
+
+interface Report {
+	[key: string]: number;
+}
+
+const headers = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Methods': 'PUT'
+};
+const allowedOrigins = ['http://localhost:4321'];
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		if (request.method === 'OPTIONS') {
+			// Handle CORS preflight requests
+			return handleOptions(request);
+		}
+		if (request.method === 'PUT') {
+			const body = await request.json() as { id: string, reason: string };
+			const reported: Report = await env.GIFT.get(body.id).then(obj => obj?.json() || {});
+			reported[body.reason] = (reported[body.reason] || 0) + 1;
+			await env.GIFT.put(body.id, JSON.stringify(reported));
+			return new Response(null, {status: 204, headers});
+		}
+		return new Response(null, {status: 400});
+	}
+};
+
+async function handleOptions(request: Request) {
+	const origin = request.headers.get('Origin') || '';
+	if (
+		allowedOrigins.includes(origin) &&
+		request.headers.get('Access-Control-Request-Method') !== null
+	) {
+
+		const corsHeaders = {
+			'Access-Control-Allow-Origin': origin,
+			'Access-Control-Allow-Methods': 'PUT, OPTIONS',
+			'Access-Control-Max-Age': '86400',
+		};
+
+		// Handle CORS preflight requests.
+		return new Response(null, {
+			headers: {
+				...corsHeaders,
+				'Access-Control-Allow-Headers': request.headers.get(
+					'Access-Control-Request-Headers'
+				),
+			},
+		});
+	} else {
+		return new Response(null, {
+			status: 400
+		});
+	}
+}
---
+++ src/components/ReportMenu.ts
@@ -0,0 +1,62 @@
+const checkSvg = '<svg xmlns="http://www.w3.org/2000/svg" fill="green" height="16" width="16" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg>';
+
+const TEMPLATE = `
+    <span id="icon">
+    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg>
+   </span>
+    <span id="title">Report</span>
+  <div class="card-report-menu">
+    <div class="card-report-menu__item" data-report="link">Broken Link</div>
+    <div class="card-report-menu__item" data-report="purchasable">Product Not Purchasable</div>
+    <div class="card-report-menu__item">Cancel</div>
+  </div>
+`;
+
+export default class ReportMenu extends HTMLElement {
+  static get observedAttributes() {
+    return [];
+  }
+
+  doUpdate() {
+
+  }
+
+  clickListener = (e) => {
+    e.stopPropagation();
+    const target = e.target as HTMLElement;
+    if (target.classList.contains('card-report-menu__item')) {
+      if (target.dataset.report) {
+        const card = target.closest('my-card');
+        const id = card?.getAttribute('id');
+        void fetch(`https://gift-api.randombits.workers.dev`, {
+          method: 'PUT',
+          body: JSON.stringify({
+            id,
+            reason: target.innerText,
+          })
+        });
+        this.querySelector('#title')!.innerHTML = 'Thanks!';
+        this.querySelector('#icon')!.innerHTML = checkSvg;
+        this.removeEventListener('click', this.clickListener);
+      }
+      this.toggle();
+    } else {
+      this.toggle();
+    }
+  };
+
+  connectedCallback() {
+    this.innerHTML = TEMPLATE;
+    this.doUpdate();
+    this.addEventListener('click', this.clickListener);
+  }
+
+  toggle() {
+    this.classList.toggle('show');
+  }
+
+  attributeChangedCallback() {
+    // this.style.display = this.hasAttribute('show') ? 'block' : 'none';
+  }
+}
+
