10fd3225ce805832b10cdfdb0136e773cec799c4
nienow <nienow@users.noreply.github.com>
2024-01-08 02:46:53 +0000

    deploy: 7fb5e97e541dd1857afb6446d65d60ebdd492e62

--- _astro/hoisted.Qqw1LVHe.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.xg8AAIAA.js";document.querySelectorAll(".lite-card").forEach(e=>{e.addEventListener("click",t=>{const r=t.currentTarget;window.open(r.dataset.url,"_blank")})});
---
+++ _astro/hoisted.lDPZ0S-j.js
@@ -0,0 +1,1 @@
+import"./hoisted.xg8AAIAA.js";document.querySelectorAll("[data-url]").forEach(e=>{e.addEventListener("click",t=>{const r=t.currentTarget;window.open(r.dataset.url,"_blank")})});
